48417d7cfc60294c3fc1abd14cb99564433b9f66
Bileam Scheuvens <36153336+bileamScheuvens@users.noreply.github.com>
2022-11-19 18:16:56 +0100

    symbols

--- harmonyABM.jl
+++ harmonyABM.jl
@@ -23,6 +23,8 @@ end
 space = GridSpaceSingle((10,10); periodic=false)
 
 device_args = DefaultDict(nothing)
+marker_dict = DefaultDict('*')
+color_dict = DefaultDict(:black)
 abstract type DeviceAgent <: AbstractAgent end
 interacts(agent::DeviceAgent) = nothing
 is_interacted_with(agent::DeviceAgent) = nothing
@@ -35,33 +37,61 @@ end
 device_args[Printer]=Dict(
     "paperstacks"=>0
 )
+marker_dict[Printer] = 'P'
+color_dict[Printer] = :grey
 isstatic(agent::Printer) = true
 acts(agent::Printer) = false
-function is_interaced_with(agent::Printer)
-    agent.paperstack += rand(1:5)
+function is_interacted_with(agent::Printer)
+    agent.paperstacks += rand(1:5)
 end
 
 @agent Lights GridAgent{2} DeviceAgent begin    
     macaddr::String
+    on::Bool
 end
+device_args[Lights]=Dict(
+    "on"=>false
+)
+
+marker_dict[Lights] = 'L'
+color_dict[Lights] = :orange
 isstatic(agent::Lights) = true
 acts(agent::Lights) = false
+function is_interacted_with(agent::Lights)
+    agent.on = !agent.on
+end
 
 @agent Phone GridAgent{2} DeviceAgent begin
     macaddr::String
+    sms_received::Int
+    jobs_sent::Int
 end
+device_args[Phone]=Dict(
+    "sms_received"=>0,
+    "jobs_sent"=>0
+)
+marker_dict[Phone] = '☎'
+color_dict[Phone] = :green
 isstatic(agent::Phone) = false
 acts(agent::Phone) = true
+function interacts(agent::Phone)
+    agent.jobs_sent += 1
+end
+function interacted_with(agent::Phone)
+    agent.sms_received += 1
+end
 
 @agent DesktopPC GridAgent{2} DeviceAgent begin
     macaddr::String
 end
+marker_dict[DesktopPC] = 'C'
 isstatic(agent::DesktopPC) = true
 acts(agent::DesktopPC) = true
 
 agenttypes = [Printer, Lights, Phone, DesktopPC]
 
-function initialize(;num_agents=20, griddims=(15,15), plot=false)
+function initialize(;num_agents=20, griddims=(15,15), plot=false, acting_chance=0.5)
+    global act_chance = acting_chance
     space = GridSpace(griddims, periodic=false)
     model = ABM(Union{agenttypes...}, space; warn=false)
     global g = DiGraph()
@@ -75,8 +105,9 @@ function initialize(;num_agents=20, griddims=(15,15), plot=false)
     end
     fig = nothing
     if plot
-        am = '☎'
-        fig, ax, abmops = abmplot(model; as=20, am, agent_step!)
+        getmarker(x) = marker_dict[typeof(x)]
+        getcolor(x) = color_dict[typeof(x)]
+        fig, ax, abmops = abmplot(model; ac=getcolor, as=20, am=getmarker, agent_step!)
     end
     model, fig
 end
@@ -95,7 +126,7 @@ function agent_step!(agent, model)
     end
     if acts(agent)
         for partner in nearby_agents(agent, model)
-            interact(agent, partner)
+            rand() < act_chance ? interact(agent, partner) : nothing
         end
     end
 end
@@ -112,4 +143,5 @@ begin
     model, fig = initialize(; num_agents=200, griddims=(30,30), plot=true )
     fig
 end
-dissect_components(g)
+
+model[63]
